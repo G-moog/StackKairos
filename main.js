bf1baf22b539a0b1738fb2adddba0b9512b35ae6
G-moog <kingmumuop@gmail.com>
2023-11-07 17:49:59 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function includeHTML() {
+    console.log("동작하나?");
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("include-html");
+        // valueOfFile = elmnt.include-html();
+        console.log(file);
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    elmnt.innerHTML = this.responseText;
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
